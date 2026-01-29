c7e29d42cb074ba1441cf569dba21c2bbcc23a20
Supan Adit Pratama <email@supanadit.com>
2026-01-29 13:39:50 +0700

    feat: change markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -107,16 +107,25 @@ return {
       require("render-markdown").setup {}
     end,
   },
-
-  -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  --   ft = { "markdown" },
-  --   build = "cd app && npm install", -- Manually trigger the npm install
-  --   init = function()
-  --     vim.g.mkdp_filetypes = { "markdown" }
-  --   end,
-  -- },
+  {
+    -- Install markdown preview, use npx if available.
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function(plugin)
+      if vim.fn.executable "npx" then
+        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+      else
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn["mkdp#util#install"]()
+      end
+    end,
+    init = function()
+      if vim.fn.executable "npx" then
+        vim.g.mkdp_filetypes = { "markdown" }
+      end
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
