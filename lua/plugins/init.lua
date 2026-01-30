0e55b283c6086e0b7224ad9242e1816d72546f63
Supan Adit Pratama <email@supanadit.com>
2026-01-30 13:44:27 +0700

    feat: add suround features

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -180,6 +180,16 @@ return {
       },
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
