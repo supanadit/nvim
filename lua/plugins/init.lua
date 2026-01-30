8e1e68cdfc2c642c92a557424192bc4774a94daa
Supan Adit Pratama <email@supanadit.com>
2026-01-30 11:18:30 +0700

    feat: add nvim-spectre an alternative to CTRL + F in vscode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -126,6 +126,60 @@ return {
       end
     end,
   },
+  {
+    "javiorfo/nvim-soil",
+
+    -- Optional for puml syntax highlighting:
+    dependencies = { "javiorfo/nvim-nyctophilia" },
+
+    lazy = true,
+    ft = "plantuml",
+    opts = {
+      -- If you want to change default configurations
+
+      -- This option closes the image viewer and reopen the image generated
+      -- When true this offers some kind of online updating (like plantuml web server)
+      actions = {
+        redraw = false,
+      },
+
+      -- If you want to use Plant UML jar version instead of the installed version
+      puml_jar = "/home/supanadit/SDK/PlantUML/gplv2-1.2026.1.jar",
+
+      -- If you want to customize the image showed when running this plugin
+      image = {
+        darkmode = false, -- Enable or disable darkmode
+        format = "png", -- Choose between png or svg
+
+        -- This is a default implementation of using nsxiv to open the resultant image
+        -- Edit the string to use your preferred app to open the image (as if it were a command line)
+        -- Some examples:
+        -- return "feh " .. img
+        -- return "xdg-open " .. img
+        execute_to_open = function(img)
+          return "nsxiv -b " .. img
+        end,
+      },
+    },
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre" },
+      { "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word" },
+      {
+        "<leader>sp",
+        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+        desc = "Search in current file",
+      },
+    },
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
