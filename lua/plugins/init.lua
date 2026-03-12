5fa3f78442ce4d703d632b0d2e63f13f2a0d43dd
Supan Adit Pratama <email@supanadit.com>
2026-03-12 19:45:42 +0700

    feat: always set opencode to plan mode when using neovim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -278,8 +278,19 @@ return {
     config = function()
       ---@type opencode.Opts
       vim.g.opencode_opts = {
-        -- Your configuration, if any; goto definition on the type or field for details
-      }
+      -- Your configuration, if any; goto definition on the type or field for details
+      server = {
+        start = function()
+          require("opencode.terminal").start("opencode --port --agent plan")
+        end,
+        toggle = function()
+          require("opencode.terminal").toggle("opencode --port --agent plan")
+        end,
+        stop = function()
+          require("opencode.terminal").stop()
+        end,
+      },
+    }
 
       vim.o.autoread = true -- Required for `opts.events.reload`
 
